8836a44ddf2d0ef9175f8bcd3e28ae7685919427
Pawan_Maurya <mauryapawan010011@gmail.com>
2023-08-28 01:10:20 +0530

    improved login/registration UI

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { setToken, setUser } from '../services/authSlice';
+import Footer from '../components/Footer';
 
 const LoginPage = () => {
     const dispatch = useDispatch();
@@ -23,7 +24,10 @@ const LoginPage = () => {
     }
 
     return (
-        <div className='w-[100vw] h-[100vh] grow flex items-center justify-center'>
+        <div className='w-[100vw] h-[100vh] grow flex flex-col items-center justify-start'>
+            <div className='my-12'>
+                <img className='w-88 sm:w-96 sm:h-40 h-32' src="/assets/starwars_logo.png" alt="" />
+            </div>
             <div className='w-[80vw]'>
                 <h1 className='text-white text-4xl text-center mb-4'>Login</h1>
                 <form className='max-w-lg space-y-1 mx-auto flex flex-col onSubmit={loginUser}' onSubmit={loginUser}>
@@ -41,12 +45,13 @@ const LoginPage = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                    <button className='text-white py-1 bg-[#F5385D] rounded-sm'>Login</button>
+                    <button className='text-white py-1 bg-[#F5385D] hover:bg-[#ba2f4b] rounded-sm transform transition delay-150 duration-150 ease-out'>Login</button>
                     <div className='text-center py-2 text-gray-500'>
                         Don't have an account yet? <Link to={'/register'} className='underline text-gray-500'>Register now</Link>
                     </div>
                 </form>
             </div>
+            <Footer />
         </div>
     )
 }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import Footer from '../components/Footer';
 
 const RegisterPage = () => {
     const navigate = useNavigate();
@@ -13,7 +14,10 @@ const RegisterPage = () => {
     }
 
     return (
-        <div className='w-[100vw] h-[100vh] grow flex items-center justify-center'>
+        <div className='w-[100vw] h-[100vh] grow flex flex-col items-center justify-start'>
+            <div className='my-12'>
+                <img className='w-88 sm:w-96 sm:h-40 h-32' src="/assets/starwars_logo.png" alt="" />
+            </div>
             <div className='w-[80vw]'>
                 <h1 className='text-white text-4xl text-center mb-4'>Sign Up</h1>
                 <form className='max-w-lg space-y-1 mx-auto flex flex-col' onSubmit={registerUser}>
@@ -38,12 +42,13 @@ const RegisterPage = () => {
                         onChange={e => setPassword(e.target.value)}
                         className='px-1 rounded-sm h-8'
                     />
-                    <button className='text-white py-1 bg-[#F5385D] rounded-sm'>Join</button>
+                    <button className='text-white py-1 bg-[#F5385D] rounded-sm hover:bg-[#ba2f4b] transform transition delay-150 duration-150 ease-out'>Join</button>
                     <div className='text-center py-2 text-gray-500 cursor-pointer'>
                         Already Registered? <Link to={'/login'} className='underline text-gray-500'>Sign In</Link>
                     </div>
                 </form>
             </div >
+            <Footer />
         </div >
     )
 }
